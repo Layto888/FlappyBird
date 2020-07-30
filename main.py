7e061907bdfa9b4b09172b798a402eac08fc61dc
Amine Amardjia <amardjia.amine@gmail.com>
2020-07-31 00:27:14 +0200

    update version

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ Demo 4: Flappy Bird example
 ---------------------------
 @Author: Amardjia Amine
 @Date: 10/08/18
-@Version: 0.0.1 (beta)
+@Version: 0.0.2 (beta)
 
 A simple 2D game with laylib and  pygame to show how fast and simple
 is to create a multimedia application using the laylib package.
 Note that the initialization of pygame and the resources management
 are completely transparent and automatic for the developer.
+
+This game must use obsolete laylib version 1.0 (available in the same folder of game)
+
 """
 from laylib import Environment
 from engine import FlappyBird
